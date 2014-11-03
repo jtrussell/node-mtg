36c8e8c19b050259dab90cae4c76bf23d99488e1
jtrussell <jus.russell@gmail.com>
2014-11-02 21:10:04 -0500

    chore(docs): Get docs for all the lib files

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,4 +1,4 @@
-/*global exec, config */
+/*global rm, exec, config */
 
 'use strict';
 
@@ -7,8 +7,35 @@ config.silent = true;
 
 var doxbin = '"./node_modules/.bin/dox"';
 
+rm('-rf', 'docs/json/master');
+
+// DB
+mkdir('-p', 'docs/json/master/db');
+
+exec(doxbin + ' < lib/db/index.js').output
+  .to('docs/json/master/db/index.json');
+
 exec(doxbin + ' < lib/db/load.js').output
   .to('docs/json/master/db/load.json');
 
 exec(doxbin + ' < lib/db/truncate.js').output
   .to('docs/json/master/db/truncate.json');
+
+// Card
+mkdir('-p', 'docs/json/master/card');
+
+exec(doxbin + ' < lib/card/index.js').output
+  .to('docs/json/master/card/index.json');
+
+exec(doxbin + ' < lib/card/info.js').output
+  .to('docs/json/master/card/info.json');
+
+// Deck
+mkdir('-p', 'docs/json/master/deck');
+
+exec(doxbin + ' < lib/deck/index.js').output
+  .to('docs/json/master/deck/index.json');
+
+exec(doxbin + ' < lib/deck/curve.js').output
+  .to('docs/json/master/deck/curve.json');
+
