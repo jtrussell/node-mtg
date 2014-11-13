9c38ae077d70eb4269aa58f5208d225601e437d9
jtrussell <jus.russell@gmail.com>
2014-11-12 22:51:48 -0500

    test(api): Add test stubs for db.load

---
+++ test/db/load.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+var lib = require('./util').lib
+  , fx = require('./util').fixture
+  , expect = require('chai').expect;
+
+describe('mtg.db.load', function() {
+  
+  var load = lib('db/load');
+
+  it('should be able to import an mtgjson file', function(done) {
+    // ...
+    done();
+  });
+
+  it('should overwrite duplicates (by multiverse id)', function(done) {
+    // ...
+    done();
+  });
+
+  it('should download the import file if not provided with one', function(done) {
+    // ...
+    // ... intercept the http request
+    done();
+  });
+
+  it('should have an option to include -extra- informtation', function(done) {
+    // ...
+    done();
+  });
+
+  it('should create a new database and schema from scracth', function(done) {
+    // ...
+    done();
+  });
+
+  it('should have an option to set the db file location', function(done) {
+    // ...
+    done();
+  });
+
+});
--- test/util/index.js
+++ test/util/index.js
@@ -1,9 +1,14 @@
 
 'use strict';
 
-var join = require('path').join;
+var fs = require('fs')
+  , join = require('path').join;
 
 exports.lib = function(path) {
   path = join('../../lib', path);
   return require(path);
+};
+
+exports.fixture = function(file) {
+  return fs.readFileSync(join(__dirname, 'fixtures', file)).toString();
 };
