6055531cd5899372a9a9b071e71a8e87e0a7ac9c
Justin Russell <jus.russell@gmail.com>
2014-11-19 21:43:11 -0500

    test: Add boilerplate for truncate tests

---
+++ test/db/truncate.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+var lib = require('./util').lib
+  , fx = require('./util').fixture
+  , expect = require('chai').expect;
+
+describe('mtg.db.truncate', function() {
+  var trunc = lib('db/truncate');
+
+  it('should be able to remove all data from the sqllite db', function(done) {
+    // ...
+    done();
+  });
+
+});
